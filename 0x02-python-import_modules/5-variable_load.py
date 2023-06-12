8d54d6443ba4285b097dc6d56b7c59587d55d79d
flashbat51 <georgeadomakojnr51@aol.com>
2023-06-12 01:19:17 -0700

    fixing task 5

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    from variable_load_5 import a
-    
+from variable_load_5 import a
+
+
+def find_var():
     print(a)
+
+
+if __name__ == "__main__":
+    find_var()

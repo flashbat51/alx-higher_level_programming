02f432e3050d90c927406d7770e967ed051689b7
Adomako George Junior <georgeadomakojnr51@aol.com>
2023-06-15 21:49:56 +0000

    adding task 7-12

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    """
+    function to replace or add
+    key/value in a dictionary. By Jnr
+    """
+    a_dictionary[key] = value
+    return a_dictionary
